e93d432b92305f3fa36c0cb7eaeff75fee3cdc6a
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2021-03-28 02:19:44 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ cp -f src/about.cpp /usr/local/src/gf1-gui/.
 cp -f src/about.h /usr/local/src/gf1-gui/.
 cp -f src/about.ui /usr/local/src/gf1-gui/.
 cp -f src/gf1-gui.pro /usr/local/src/gf1-gui/.
+cp -f src/GPL.txt /usr/local/src/gf1-gui/.
 cp -f src/icons/active64.png /usr/local/src/gf1-gui/icons/.
 cp -f src/icons/greyed64.png /usr/local/src/gf1-gui/icons/.
 cp -f src/icons/selected64.png /usr/local/src/gf1-gui/icons/.
@@ -19,6 +20,9 @@ cp -f src/mainwindow.h /usr/local/src/gf1-gui/.
 cp -f src/mainwindow.ui /usr/local/src/gf1-gui/.
 cp -f src/resources.qrc /usr/local/src/gf1-gui/.
 cp -f src/README.txt /usr/local/src/gf1-gui/.
+cp -f src/serial.cpp /usr/local/src/gf1-gui/.
+cp -f src/serial.h /usr/local/src/gf1-gui/.
+cp -f src/serial.ui /usr/local/src/gf1-gui/.
 echo Building and installing application...
 cd /usr/local/src/gf1-gui
 qmake
