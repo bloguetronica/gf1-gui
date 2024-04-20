573ca31b59d7e80a0713f122d1011ec2bd46131e
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2024-04-21 00:06:32 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,18 +7,21 @@ apt-get -qq install qt5-default
 apt-get -qq install qtbase5-dev
 echo Copying source code files...
 mkdir -p /usr/local/src/gf1-gui/icons
+mkdir -p /usr/local/src/gf1-gui/misc
 cp -f src/about.cpp /usr/local/src/gf1-gui/.
 cp -f src/about.h /usr/local/src/gf1-gui/.
 cp -f src/about.ui /usr/local/src/gf1-gui/.
 cp -f src/gf1-gui.pro /usr/local/src/gf1-gui/.
 cp -f src/GPL.txt /usr/local/src/gf1-gui/.
 cp -f src/icons/active64.png /usr/local/src/gf1-gui/icons/.
+cp -f src/icons/gf1-gui.png /usr/local/src/gf1-gui/icons/.
 cp -f src/icons/greyed64.png /usr/local/src/gf1-gui/icons/.
 cp -f src/icons/selected64.png /usr/local/src/gf1-gui/icons/.
 cp -f src/main.cpp /usr/local/src/gf1-gui/.
 cp -f src/mainwindow.cpp /usr/local/src/gf1-gui/.
 cp -f src/mainwindow.h /usr/local/src/gf1-gui/.
 cp -f src/mainwindow.ui /usr/local/src/gf1-gui/.
+cp -f src/misc/gf1-gui.desktop /usr/local/src/gf1-gui/misc/.
 cp -f src/resources.qrc /usr/local/src/gf1-gui/.
 cp -f src/README.txt /usr/local/src/gf1-gui/.
 cp -f src/serial.cpp /usr/local/src/gf1-gui/.
